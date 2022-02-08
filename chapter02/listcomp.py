7ccd7dcf774913c370a37f4c2758598a89995bcd
Daniel Theodor Plop <1534513+plopd@users.noreply.github.com>
2022-02-08 22:37:25 +0100

    Listcomps Versus map and filter

--- chapter02/listcomp.py
+++ chapter02/listcomp.py
@@ -1,3 +1,8 @@
+"""
+    Source: “Fluent Python by Luciano Ramalho (O’Reilly). Copyright 2015 Luciano Ramalho, 978-1-491-94600-8.”
+"""
+
+## Listcomps No Longer Leak Their Variables
 
 x = "ABC"
 # Variables in the surrounding scope can still be referenced
@@ -5,3 +10,11 @@ dummy = [ord(x) for x in x]
 # The x inside the listcomp does not mask the x in the surrounding scope
 print(x) # ABC
 print(dummy)  # [65, 66, 67]
+
+## Listcomps Versus map and filter
+## Example 2-3. The same list built by a listcomp and a map/filter composition
+symbols = '$¢£¥€¤'
+beyond_ascii = [ord(s) for s in symbols if ord(s) > 127]
+print(beyond_ascii) # [162, 163, 165, 8364, 164]
+beyond_ascii = list(filter(lambda c: c > 127, map(ord, symbols)))
+print(beyond_ascii) # [162, 163, 165, 8364, 164]
