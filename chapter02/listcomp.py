93745c9e00c8c9295c178332f5b20644d3259db6
Daniel Theodor Plop <1534513+plopd@users.noreply.github.com>
2022-04-14 21:42:09 +0200

    Add example

--- chapter02/listcomp.py
+++ chapter02/listcomp.py
@@ -52,3 +52,26 @@ for passport in sorted(travel_ids):
 
 for country, _ in travel_ids:
     print(country)
+
+# Some cool stuff with tuple unpacking
+
+# Parallel tuple unpacking using * notation to grab the excess.
+# Note that only one item can have the *, but it may appear in any position of the unpacking.
+a, b, *rest = range(2)
+print(a, b, rest) # 0, 1, []
+
+
+# Example 2-8. Unpacking nested tuples to access the longitude
+metro_areas = [
+    ('Tokyo','JP',36.933,(35.689722,139.691667)), 
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
+
+print("{:15} | {:^9} | {:^9}".format("city", "lat.", "long."))
+print("-"*42)
+fmt = "{:15} | {:9.4f} | {:9.4f}"
+for city, cc, pop, (latitude, longitude) in metro_areas:
+    print(fmt.format(city, latitude, longitude))
