bd4f9802816f4ec957ad7a47c1183178394d86e4
Daniel Theodor Plop <1534513+plopd@users.noreply.github.com>
2022-04-15 09:46:32 +0200

    Move decorator in own module

--- chapter07/07-15.py
+++ chapter07/07-15.py
@@ -5,29 +5,7 @@
     Example 7-15. A simple decorator to output the running time of functions
 """
 import time
-import functools
-
-def clock(func):
-    """This is a simple decorator function."""
-    @functools.wraps(func)
-    def clocked(*args, **kwargs):
-        """This is a closure function that extends the output of a function with its running time."""
-        t0 = time.perf_counter()
-        result = func(*args, **kwargs)
-        elapsed = time.perf_counter() - t0
-        name = func.__name__
-        arg_lst = []
-        if args:
-            arg_lst.append(', '.join(repr(arg) for arg in args))
-        if kwargs:
-            pairs = ['%s=%s' % (k, w) for k, w in sorted(kwargs.items())]
-            arg_lst.append(', '.join(pairs))
-        arg_str = ', '.join(arg_lst)
-        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
-
-        return result
-        
-    return clocked
+from clockdeco import clock
 
 @clock
 def snooze(seconds):
